a3a106c098110bbbfffe4e3050646687b9a0990a
jamahadrummer <jamahadrummer@live.com>
2021-09-23 20:35:07 -0500

    prettier

--- main.js
+++ main.js
@@ -7,50 +7,50 @@ const join5 = document.getElementById("j5");
 
 
 function cresInteract(num1) {
-    console.log("Button has been Pushed");
-    crComLib.publishEvent("b", num1 , "true");
-    console.log(num1,"High");
-    setTimeout(() => {
-        crComLib.publishEvent("b", num1 , "false");
-    }, 2000);
-    console.log(num1,"Low")
-  };
+  console.log("Button has been Pushed");
+  crComLib.publishEvent("b", num1, "true");
+  console.log(num1, "High");
+  setTimeout(() => {
+    crComLib.publishEvent("b", num1, "false");
+  }, 2000);
+  console.log(num1, "Low")
+};
 
-  join1.addEventListener("click", () => {
-    cresInteract("41");
-    setTimeout (function() {
+join1.addEventListener("click", () => {
+  cresInteract("41");
+  setTimeout(function () {
     //   pageTurn("localPC.html");
-    }, 1000);
-    
-  });
+  }, 1000);
 
-  join2.addEventListener("click", () => {
-    cresInteract("41");
-    setTimeout (function() {
-    //   pageTurn("localPC.html");
-    }, 1000);
-    
-  });
-  join3.addEventListener("click", () => {
-    cresInteract("41");
-    setTimeout (function() {
-    //   pageTurn("localPC.html");
-    }, 1000);
-    
-  });
+});
 
-  join4.addEventListener("click", () => {
-    cresInteract("41");
-    setTimeout (function() {
+join2.addEventListener("click", () => {
+  cresInteract("41");
+  setTimeout(function () {
     //   pageTurn("localPC.html");
-    }, 1000);
-    
-  });
+  }, 1000);
 
-  join5.addEventListener("click", () => {
-    cresInteract("41");
-    setTimeout (function() {
+});
+join3.addEventListener("click", () => {
+  cresInteract("41");
+  setTimeout(function () {
     //   pageTurn("localPC.html");
-    }, 1000);
-    
-  });
+  }, 1000);
+
+});
+
+join4.addEventListener("click", () => {
+  cresInteract("41");
+  setTimeout(function () {
+    //   pageTurn("localPC.html");
+  }, 1000);
+
+});
+
+join5.addEventListener("click", () => {
+  cresInteract("41");
+  setTimeout(function () {
+    //   pageTurn("localPC.html");
+  }, 1000);
+
+});
