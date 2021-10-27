a3874b9776956545e8ce469121cb9fd61c1160ff
jamahadrummer <jamahadrummer@live.com>
2021-10-27 11:18:16 -0500

    still no crComLib success

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import * as crComLib from "@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib.js";
+// import * as crComLib from "@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib.js";
+import { publishEvent } from '/node_modules/@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib.js';
 const join1 = document.getElementById("j1");
 const join2 = document.getElementById("j2");
 const join3 = document.getElementById("j3");
@@ -8,49 +9,44 @@ const join5 = document.getElementById("j5");
 
 function cresInteract(num1) {
 	console.log("Button has been Pushed");
-	crComLib.publishEvent("b", num1, "true");
+	publishEvent("b", num1, "true");
 	console.log(num1, "High");
 	setTimeout(() => {
-		crComLib.publishEvent("b", num1, "false");
+		publishEvent("b", num1, "false");
 	}, 2000);
 	console.log(num1, "Low");
 };
 
 join1.addEventListener("click", () => {
-	cresInteract("41");
+	cresInteract(1);
 	setTimeout(function () {
-		//   pageTurn("localPC.html");
 	}, 1000);
 
 });
 
 join2.addEventListener("click", () => {
-	cresInteract("41");
+	cresInteract(2);
 	setTimeout(function () {
-		//   pageTurn("localPC.html");
 	}, 1000);
 
 });
 join3.addEventListener("click", () => {
-	cresInteract("41");
+	cresInteract(3);
 	setTimeout(function () {
-		//   pageTurn("localPC.html");
 	}, 1000);
 
 });
 
 join4.addEventListener("click", () => {
-	cresInteract("41");
+	cresInteract(4);
 	setTimeout(function () {
-		//   pageTurn("localPC.html");
 	}, 1000);
 
 });
 
 join5.addEventListener("click", () => {
-	cresInteract("41");
+	cresInteract(5);
 	setTimeout(function () {
-		//   pageTurn("localPC.html");
 	}, 1000);
 
 });
