cabb79a0315d1a71410c4e131d2ed5dc8b94e4ee
jamahadrummer <jamahadrummer@live.com>
2021-11-10 15:50:25 -0600

    crcomlib publish event working

--- src/app.js
+++ src/app.js
@@ -26,6 +26,124 @@ window.bridgeReceiveBooleanFromNative = crComLib.bridgeReceiveBooleanFromNative;
 window.bridgeReceiveStringFromNative = crComLib.bridgeReceiveStringFromNative;
 window.bridgeReceiveObjectFromNative = crComLib.bridgeReceiveObjectFromNative;
 
+// // import * as crComLib from '@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib.js';
+// // import { publishEvent } from '/node_modules/@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib.js';
+// import { subscribeState } from '@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib';
+// import { publishEvent } from '@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib';
+// // const crComLib = require('@crestron/ch5-crComLib')
+const join1 = document.getElementById('j1');
+const join2 = document.getElementById('j2');
+const join3 = document.getElementById('j3');
+const join4 = document.getElementById('j4');
+const join5 = document.getElementById('j5');
+const read1 = document.getElementById('s1');
+const read2 = document.getElementById('s2');
+const read3 = document.getElementById('s3');
+const read4 = document.getElementById('s4');
+const read5 = document.getElementById('s5');
+
+// function cresInteract(num1) {
+// 	console.log('Button has been Pushed');
+// 	crComLib.publishEvent('b', num1, 'true');
+// 	console.log(num1, 'High');
+// 	setTimeout(() => {
+// 		crComLib.publishEvent('b', num1, 'false');
+// 	}, 2000);
+// 	console.log(num1, 'Low');
+// };
+
+
+function cresRead(num1, elem) {
+	crComLib.subscribeState('s', num1, (value) =>
+		elem.innerHTML = value);
+};
+
+
+function onClick1(digitalJoin) {
+	crComLib.publishEvent('b', digitalJoin, true);
+	crComLib.publishEvent('b', digitalJoin, false);
+}
+join1.addEventListener('click', onClick1('1'));
+join2.addEventListener('click', onClick1('2'));
+join3.addEventListener('click', onClick1('3'));
+join4.addEventListener('click', onClick1('4'));
+// join5.addEventListener('click', onClick('5'));
+
+function digiOne(ev) {
+	crComLib.publishEvent('b', '1', true);
+	crComLib.publishEvent('b', '1', false);
+}
+
+function digiTwo(ev) {
+	crComLib.publishEvent('b', '2', true);
+	crComLib.publishEvent('b', '2', false);
+}
+
+function digiThree(ev) {
+	crComLib.publishEvent('b', '3', true);
+	crComLib.publishEvent('b', '3', false);
+}
+
+function digiFour(ev) {
+	crComLib.publishEvent('b', '4', true);
+	crComLib.publishEvent('b', '4', false);
+}
+
+function digiFive(ev) {
+	crComLib.publishEvent('b', '5', true);
+	crComLib.publishEvent('b', '5', false);
+}
+
+join1.addEventListener('click', digiOne);
+join2.addEventListener('click', digiTwo);
+join3.addEventListener('click', digiThree);
+join4.addEventListener('click', digiFour);
+join5.addEventListener('click', digiFive);
+
+
+cresRead('1', read1);
+cresRead('2', read2);
+cresRead('1', read3);
+cresRead('4', read4);
+cresRead('5', read5);
+
+// join1.addEventListener('click', () => {
+// 	cresInteract('1');
+// 	setTimeout(function () {
+// 	}, 1000);
+
+// });
+
+// join2.addEventListener('click', () => {
+// 	cresInteract('2');
+// 	setTimeout(function () {
+// 	}, 1000);
+
+// });
+// join3.addEventListener('click', () => {
+// 	cresInteract('3');
+// 	setTimeout(function () {
+// 	}, 1000);
+
+// });
+
+// join4.addEventListener('click', () => {
+// 	cresInteract('4');
+// 	setTimeout(function () {
+// 	}, 1000);
+
+// });
+
+// join5.addEventListener('click', () => {
+// 	cresInteract('5');
+// 	setTimeout(function () {
+// 	}, 1000);
+
+// });
+
+
+
+
 // crComLib.subscribeState('s', '1', (value) => {
 //     const elem = document.getElementById('room-name');
 //     elem.innerHTML = value;
@@ -128,98 +246,32 @@ window.bridgeReceiveObjectFromNative = crComLib.bridgeReceiveObjectFromNative;
 //     lblDate.innerText = moment().format('dddd, MMMM Do, YYYY');
 // }, 5000);
 
+// crComLib.subscribeState('s', '1', (value) => {
+// 	const elem = document.getElementById('s1');
+// 	elem.innerHTML = value;
+// 	console.log(value);
+// });
 
-// // import * as crComLib from "@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib.js";
-// // import { publishEvent } from '/node_modules/@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib.js';
-// import { subscribeState } from '@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib';
-// import { publishEvent } from '@crestron/ch5-crComLib/build_bundles/cjs/cr-com-lib';
-// // const crComLib = require('@crestron/ch5-crComLib')
-const join1 = document.getElementById("j1");
-const join2 = document.getElementById("j2");
-const join3 = document.getElementById("j3");
-const join4 = document.getElementById("j4");
-const join5 = document.getElementById("j5");
-const read1 = document.getElementById("s1");
-const read2 = document.getElementById("s2");
-const read3 = document.getElementById("s3");
-const read4 = document.getElementById("s4");
-const read5 = document.getElementById("s5");
+// crComLib.subscribeState('s', '2', (value) => {
+// 	const elem = document.getElementById('s2');
+// 	elem.innerHTML = value;
+// 	console.log(value);
+// });
 
+// crComLib.subscribeState('s', '3', (value) => {
+// 	const elem = document.getElementById('s3');
+// 	elem.innerHTML = value;
+// 	console.log(value);
+// });
 
-crComLib.subscribeState('s', '1', (value) => {
-	const elem = document.getElementById('s1');
-	elem.innerHTML = value;
-	console.log(value);
-});
+// crComLib.subscribeState('s', '4', (value) => {
+// 	const elem = document.getElementById('s4');
+// 	elem.innerHTML = value;
+// 	console.log(value);
+// });
 
-crComLib.subscribeState('s', '2', (value) => {
-	const elem = document.getElementById('s2');
-	elem.innerHTML = value;
-	console.log(value);
-});
-
-crComLib.subscribeState('s', '3', (value) => {
-	const elem = document.getElementById('s3');
-	elem.innerHTML = value;
-	console.log(value);
-});
-
-crComLib.subscribeState('s', '4', (value) => {
-	const elem = document.getElementById('s4');
-	elem.innerHTML = value;
-	console.log(value);
-});
-
-crComLib.subscribeState('s', '5', (value) => {
-	const elem = document.getElementById('s5');
-	elem.innerHTML = value;
-	console.log(value);
-});
-
-// function cresRead(num1) {
-// 	subscribeState('s', num1, (value) )
-// }
-
-function cresInteract(num1) {
-	console.log("Button has been Pushed");
-	crComLib.publishEvent("b", num1, "true");
-	console.log(num1, "High");
-	setTimeout(() => {
-		crComLib.publishEvent("b", num1, "false");
-	}, 2000);
-	console.log(num1, "Low");
-};
-
-join1.addEventListener("click", () => {
-	cresInteract(1);
-	setTimeout(function () {
-	}, 1000);
-
-});
-
-join2.addEventListener("click", () => {
-	cresInteract(2);
-	setTimeout(function () {
-	}, 1000);
-
-});
-join3.addEventListener("click", () => {
-	cresInteract(3);
-	setTimeout(function () {
-	}, 1000);
-
-});
-
-join4.addEventListener("click", () => {
-	cresInteract(4);
-	setTimeout(function () {
-	}, 1000);
-
-});
-
-join5.addEventListener("click", () => {
-	cresInteract(5);
-	setTimeout(function () {
-	}, 1000);
-
-});
+// crComLib.subscribeState('s', '5', (value) => {
+// 	const elem = document.getElementById('s5');
+// 	elem.innerHTML = value;
+// 	console.log(value);
+// });
